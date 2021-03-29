ead090c7a4aa6b3ebe597ad22782241be2fbef8e
johnb8005 <johnb8005@users.noreply.github.com>
2021-03-29 06:53:48 +0000

    deploy: 04fb7a962d65bbc3d010c0cc4317f3dd1b90af7b

---
+++ precache-manifest.026e4da10c8618852c2abadf0a6cc0af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08627a0b95fd7accf15152c0316c997",
+    "url": "/sudoku-ts/index.html"
+  },
+  {
+    "revision": "eb108a6f979c0d8f5c84",
+    "url": "/sudoku-ts/static/js/2.ddc464a9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sudoku-ts/static/js/2.ddc464a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17bcf2967d3a45c8b748",
+    "url": "/sudoku-ts/static/js/main.514c824b.chunk.js"
+  },
+  {
+    "revision": "b84c8a450ce041de75a4",
+    "url": "/sudoku-ts/static/js/runtime-main.f06b1e04.js"
+  }
+]);
